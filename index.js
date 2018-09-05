55b37661b0d97e98bf8309643825f6cb3e80b5f1
Edwin Moses Isang <edmosesisang@gmail.com>
2018-09-05 21:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function shout(string) {
   return string.toUpperCase()
 };
-shout("sing"); 
+shout("sing");
 
 
 
@@ -19,5 +19,3 @@ logShout("sing")
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
-
-
